d94f67f2fda27ecce51f602348eb3459d8ad0da8
siobhain <sobrien@live.ie>
2023-02-03 16:02:16 +0000

    js Add DOMloaded listener & button listeners for a click to runG

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,43 @@
 console.log("Connected");
 
-function runGame() {
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    // alert("loaded");
+
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            let userHand= this.getAttribute("data-type")
+            // alert(`you pressed ${userHand}`);    
+            runGame(userHand);
+        })
+    }
+})
+
+function runGame(userHand) {
+    let deviceHand = Math.floor(Math.random() * 3);
+    alert(`device picked ${deviceHand}`);
 }
 
-function findWinner (userHand,deviceHand) {
+function findWinner(userHand, deviceHand) {
 
 }
 
 function incrementDeviceWins() {
 
 }
+
 function incrementUserWins() {
 
 }
+
 function incrementDraw() {
 
 }
+
 function displayResult() {
 
 }
-
