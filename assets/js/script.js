f1c07237f7d974b3075cad2cf2e4f3af4d67499a
siobhain <sobrien@live.ie>
2023-02-03 18:20:30 +0000

    runGame : deviceHand & userHand randomally assigned so as to develop game logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ console.log("Connected");
 
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
-
+/*
 document.addEventListener("DOMContentLoaded", function () {
     // alert("loaded");
 
@@ -16,13 +16,31 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     }
 })
+*/
+runGame("paper");
 
 function runGame(userHand) {
-    let deviceHand = Math.floor(Math.random() * 3);
-    alert(`device picked ${deviceHand}`);
+    const handArray = ["rock", "paper", "scissors"];
+    let deviceHand = handArray[Math.floor(Math.random() * 3)];
+    userHand = handArray[Math.floor(Math.random() * 3)];
+    console.log(`device picks ${deviceHand}`);
+    console.log(`user picks ${userHand}`);
+    if (deviceHand === userHand) {
+        console.log("Its a draw ++");
+    } else {
+        findWinner(userHand, deviceHand);
+    }
 }
 
 function findWinner(userHand, deviceHand) {
+    const rpsData = ["Rock blunts Scissors", "Paper wraps Rock", "Scissors cuts Paper", "Paper cut by Scissors", "Rock covered by Paper", "Scissors blunted by Rock"];
+
+    let userLowercaseHand = (userHand);
+    // userLowercaseHand = toLowerCase(userHand);
+    let deviceLowerHand = rpsData[2];
+    console.log(deviceLowerHand[0]);
+
+
 
 }
 
