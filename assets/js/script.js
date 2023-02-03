975c4684d6626c32c3e6c99c3f86ee7bd2589402
siobhain <sobrien@live.ie>
2023-02-03 21:55:41 +0000

    findWinner : add switch/case statement for RPS options

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,21 +27,44 @@ function runGame(userHand) {
     console.log(`user picks ${userHand}`);
     if (deviceHand === userHand) {
         console.log("Its a draw ++");
+        incrementDraw();
     } else {
-        findWinner(userHand, deviceHand);
+        let resultText = findWinner(userHand, deviceHand);
     }
 }
 
 function findWinner(userHand, deviceHand) {
-    const rpsData = ["Rock blunts Scissors", "Paper wraps Rock", "Scissors cuts Paper", "Paper cut by Scissors", "Rock covered by Paper", "Scissors blunted by Rock"];
+    const rpsData = ["Rock blunts Scissors", "Paper wraps Rock", "Scissors cuts Paper", "Rock covered by Paper", "Paper cut by Scissors", "Scissors blunted by Rock"];
 
-    let userLowercaseHand = (userHand);
     // userLowercaseHand = toLowerCase(userHand);
-    let deviceLowerHand = rpsData[2];
-    console.log(deviceLowerHand[0]);
-
-
-
+    // console.log(deviceLowerHand[0]);
+   
+    let result = userHand.concat(" ",deviceHand);
+    let text = "";
+        switch (result) {
+        case "rock paper":
+            text += "You win - rock covers paper";
+            break;
+        case "rock scissors":
+            text += "You lose - rock blunted by scissors";
+            break;
+        case "paper rock":
+            text += "You win -paper wraps rock";
+            break;
+        case "paper scissors":
+            text += "You lose -  paper cut by scissors";
+            break;
+        case "scissors rock":
+            text += "You lose - scissors blunted by rock";
+            break;
+        case "scissors paper":
+            text += "You win - scissors cuts paper";
+            break;
+        default:
+            text += "Something went wrong ...Aborting";
+        }
+        return(text);
+        console.log(text);  
 }
 
 function incrementDeviceWins() {
