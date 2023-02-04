31e7fd239777d3ee650c1bc831d8a137aab03c48
siobhain <sobrien@live.ie>
2023-02-04 00:58:50 +0000

    js: Fill incrementDraw function, update runGame with resultText & add winorlose to html/css for resultText

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ console.log("Connected");
 
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
-/*
 document.addEventListener("DOMContentLoaded", function () {
     // alert("loaded");
 
@@ -16,68 +15,77 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     }
 })
-*/
-runGame("paper");
+
+// runGame("paper");
 
 function runGame(userHand) {
     const handArray = ["rock", "paper", "scissors"];
+    let resultText = "Empty;"
     let deviceHand = handArray[Math.floor(Math.random() * 3)];
-    userHand = handArray[Math.floor(Math.random() * 3)];
+    // userHand = handArray[Math.floor(Math.random() * 3)];
     console.log(`device picks ${deviceHand}`);
     console.log(`user picks ${userHand}`);
     if (deviceHand === userHand) {
-        console.log("Its a draw ++");
+        resultText = "Its a draw ++";
+        console.log(resultText);
         incrementDraw();
     } else {
-        let resultText = findWinner(userHand, deviceHand);
-    }
+        resultText = findWinner(userHand, deviceHand);
+    } 
+    document.getElementById("winorlose").innerText=resultText;
+    console.log(resultText);
 }
 
 function findWinner(userHand, deviceHand) {
-    const rpsData = ["Rock blunts Scissors", "Paper wraps Rock", "Scissors cuts Paper", "Rock covered by Paper", "Paper cut by Scissors", "Scissors blunted by Rock"];
+    // const rpsData = ["Rock blunts Scissors", "Paper wraps Rock", "Scissors cuts Paper", "Rock covered by Paper", "Paper cut by Scissors", "Scissors blunted by Rock"];
 
     // userLowercaseHand = toLowerCase(userHand);
     // console.log(deviceLowerHand[0]);
    
     let result = userHand.concat(" ",deviceHand);
-    let text = "";
+    let rText = "";
         switch (result) {
         case "rock paper":
-            text += "You win - Rock covers paper";
+            rText += "You win - Rock covers paper";
             break;
         case "rock scissors":
-            text += "You lose - Rock blunted by scissors";
+            rText += "You lose - Rock blunted by scissors";
             break;
         case "paper rock":
-            text += "You win -Paper wraps rock";
+            rText += "You win -Paper wraps rock";
             break;
         case "paper scissors":
-            text += "You lose -  Paper cut by scissors";
+            rText += "You lose -  Paper cut by scissors";
             break;
         case "scissors rock":
-            text += "You lose - Scissors blunted by rock";
+            rText += "You lose - Scissors blunted by rock";
             break;
         case "scissors paper":
-            text += "You win - Scissors cuts paper";
+            rText += "You win - Scissors cuts paper";
             break;
         default:
-            text += "Something went wrong ...Please try again";
+            rText += "Something went wrong ...Please try again";
         }
-        return(text);
-        console.log(text);  
+    console.log(rText);  
+    return(rText);
 }
+ 
 
 function incrementDeviceWins() {
-    oldScore = document.getElementById("draw")
+    let oldScore = parseInt(document.getElementById("device").innerText);
+    document.getElementById("draw").innerText = ++oldScore;
 
 }
 
 function incrementUserWins() {
-
+    let oldScore = parseInt(document.getElementById("user").innerText);
+    document.getElementById("user").innerText = ++oldScore;
+    
 }
 
 function incrementDraw() {
-
+    let oldScore = parseInt(document.getElementById("draw").innerText);
+    document.getElementById("draw").innerText = ++oldScore;
 }
 
 function displayResult() {
