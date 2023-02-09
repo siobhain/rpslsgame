ac23d84a482ea52b84abaa816a06c5fd68fbab90
siobhain <sobrien@live.ie>
2023-02-09 00:52:29 +0000

    js Add helpButton & modal handling once dom loaded - not completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,44 @@ console.log("Connected");
 // Once DOM is loaded add eventlisteners to rpsls buttons waiting for a click
 
 document.addEventListener("DOMContentLoaded", function () {
-  // alert("loaded");
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var helpButton = document.getElementById("helpButton");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  alert("loaded");
+  //have few extra buttons that i need to cater for ie help button and play agan button
   let buttons = document.getElementsByTagName("button");
+  console.log("buttons");
+  // alert(buttons);
   for (let button of buttons) {
-    button.addEventListener("click", function () {
-      let userHand = this.getAttribute("data-type");
-      runGame(userHand);
-    });
+    console.log(button);
+    // button.addEventListener("click", function () {
+    //   let userHand = this.getAttribute("data-type");
+    //   runGame(userHand);
+   // });
   }
+  // When the user clicks the button, open the modal 
+  helpButton.onclick = function () {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
 });
 
 // runGame("paper");
